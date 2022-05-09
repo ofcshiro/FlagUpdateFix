4b73359a6c103acb91bd924ba9048ea0f3daf3ad
ofcshiro <105174877+ofcshiro@users.noreply.github.com>
2022-05-09 19:04:15 +0200

    Paleto Fix added
    Fix for the Paleto Bank added, Config File added.

---
+++ config.lua
@@ -0,0 +1,5 @@
+Config                  = {}
+
+Config.PaletoBank       = true           -- Set it to "false" if you dont use Gabz Paletobank
+Config.PacificBank      = true           -- Set it to "false" if you dont use Gabz Pacificbank
+Config.PolyZone         = true           -- DO NOT CHANGE! PolyZone as Optional will be added soon!
