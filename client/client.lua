5ca5e710ecbc3784fdb433d60e86e37057991353
ofcshiro <105174877+ofcshiro@users.noreply.github.com>
2023-03-20 01:06:48 +0100

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -1,8 +1,5 @@
--- Pacific Bank Fix
-
-if Config.PacificBank then
-
-    if Config.PolyZone then
+if Config.PolyZone then 
+    if Config.PacificBank then 
         -- Creates the PolyZone for the Pacific Bank
         PacificBank = BoxZone:Create(vector3(259.16, 223.01, 161.63), 50.2, 30.2, {
             name = "PacificBank",
@@ -10,8 +7,6 @@ if Config.PacificBank then
             debugPoly = false
         })
     
-        local inPacificBank = false
-
         -- Updates the portalflag if a player enters the polyzone
         PacificBank:onPlayerInOut(function(isInside)
             local coords = GetEntityCoords(PlayerPedId())
@@ -21,56 +16,17 @@ if Config.PacificBank then
             if isInside and portalFlag == 64 then 
                 SetInteriorPortalFlag(interiorId, 19, 0)
                 RefreshInterior(interiorId)
-                inPacificBank = true
-            elseif isInside and portalFlag == 0 then
-                inPacificBank = true
-            else
-                inPacificBank = false
             end 
         end)    
     end
 
-
-    if not Config.PolyZone then
-        local zone = {x = 262.87, y = 219.67, z = 106.26}
-        local ZoneVec = vector3(262.87,219.67,106.26)
-        local interiorId = GetInteriorAtCoords(zone.x, zone.y, zone.z)
-
-        Citizen.CreateThread(function()
-            while true do
-                Wait(1)
-                local portalFlag = GetInteriorPortalFlag(interiorId, 19)
-                local PlayerVec = GetEntityCoords(PlayerPedId())
-                local dist = #(PlayerVec.xy - ZoneVec.xy)
-                if dist <= 10 and portalFlag == 64 then 
-                    SetInteriorPortalFlag(interiorId, 19, 0)
-                    RefreshInterior(interiorId)
-                elseif dist <= 10 and portalFlag == 0 then
-                else
-                end 
-            end
-        end)
-    end
-end
-
-if not Config.PacificBank then
--- Does nothing because Pacific Bank is disabled
-end
-
-
--- Paleto Bank Fix
-
-if Config.PaletoBank then 
-
-    if Config.PolyZone then 
+    if Config.PaletoBank then 
         -- Creates a PolyZone inside of Paleto Bank
         PaletoBank = BoxZone:Create(vector3(-104.72, 6469.24, 32.08), 16.2, 15.0, {
             name = "PaletoBank",
             heading = 315,
             debugPoly = false
         })
-
-        local inPaletoBank = false
 
         -- Updates portalflag if player enters the polyzone
         PaletoBank:onPlayerInOut(function(isInside)
@@ -81,22 +37,39 @@ if Config.PaletoBank then
             if isInside and portalFlag == 64 then
                 SetInteriorPortalFlag(interiorId, 2, 0)
                 RefreshInterior(interiorId)
-                inPaletoBank = true
-            elseif isInside and portlaFlag == 0 then
-            else
             end  
         
         end)
     end
+else
+    if Config.PacificBank then 
+        local zone = {x = 262.87, y = 219.67, z = 106.26}
+        local ZoneVec = vector3(262.87,219.67,106.26)
+        local interiorId = GetInteriorAtCoords(zone.x, zone.y, zone.z)
 
-    if not Config.PolyZone then
+        Citizen.CreateThread(function()
+            while true do
+                local portalFlag = GetInteriorPortalFlag(interiorId, 19)
+                local PlayerVec = GetEntityCoords(PlayerPedId())
+                local dist = #(PlayerVec.xy - ZoneVec.xy)
+
+                if dist <= 10 and portalFlag == 64 then 
+                    SetInteriorPortalFlag(interiorId, 19, 0)
+                    RefreshInterior(interiorId)
+                end 
+
+                Wait(1500)
+            end
+        end)
+    end
+
+    if Config.PaletoBank then 
         local zone = {x = -104.37, y = 6468.01, z = 31.62}
         local ZoneVec = vector3(-104.37,6468.01,31.62)
         local interiorId = GetInteriorAtCoords(zone.x, zone.y, zone.z)
 
         Citizen.CreateThread(function()
             while true do
-                Wait(2)
                 local portalFlag = GetInteriorPortalFlag(interiorId, 2)
                 local PlayerVec = GetEntityCoords(PlayerPedId())
                 local dist = #(PlayerVec.xy - ZoneVec.xy)
@@ -104,15 +77,10 @@ if Config.PaletoBank then
                 if dist <= 8 and portalFlag == 64 then
                     SetInteriorPortalFlag(interiorId, 2, 0)
                     RefreshInterior(interiorId)
-                elseif dist <= 8 and portalFlag == 0 then
-                    -- Does Nothing
-                else
                 end
+
+                Wait(1500)
             end
         end)
     end
-end
-
-if not Config.PaletoBank then
--- Does nothing because Paleto Bank is disabled in Config.
 end
